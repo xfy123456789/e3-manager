8e61ab7af9a8d733772e2d1208b6d54d8b3bfb1d
xfy1233 <x983796531@163.com>
2018-06-20 18:07:51 +0800

    1

--- e3-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -2,7 +2,9 @@ package cn.e3mall.service;
 
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.common.pojo.*;
+import cn.e3mall.common.util.E3Result;
 public interface ItemService {
 	TbItem getItemById(long itemId);
 	EasyUIDataGridResult getItemList(int page,int rows);
+	E3Result addItem(TbItem item,String desc);
 }
--- e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +10,52 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.util.E3Result;
+import cn.e3mall.common.util.IDUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemCat;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService {
 	@Autowired
-	public TbItemMapper TbItemMapper;
+	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	@Override
 	public TbItem getItemById(long itemId) {
-	    TbItem tbItem = TbItemMapper.selectByPrimaryKey(itemId);
+	    TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
 		return tbItem;
 	}
 	@Override
 	public EasyUIDataGridResult getItemList(int page, int rows) {
 		PageHelper.startPage(page, rows);
 		TbItemExample tbItemExample=new TbItemExample();
-		List<TbItem> list = TbItemMapper.selectByExample(tbItemExample);
+		List<TbItem> list = itemMapper.selectByExample(tbItemExample);
 		PageInfo<TbItem>pageInfo=new PageInfo<>(list);
 		EasyUIDataGridResult result=new EasyUIDataGridResult();
 		result.setTotal(pageInfo.getTotal());
 		result.setRows(list);
 		return result;
+	}
+	@Override
+	public E3Result addItem(TbItem item, String desc) {
+		long itemId=IDUtils.genItemId();
+		item.setId(itemId);
+		//1-正常 ，2-下架，3-删除
+		item.setStatus((byte)1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		itemMapper.insert(item);
+		TbItemDesc itemDesc=new TbItemDesc();
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(new Date());
+		itemDesc.setUpdated(new Date());
+		itemDescMapper.insert(itemDesc);
+		return E3Result.ok();
 	}
 }
